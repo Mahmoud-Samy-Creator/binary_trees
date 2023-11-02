fd8e2f2b1dfeeadb90ef863fa205c52b9ed2c0fd
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-02 04:29:36 +0000

    10. Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,14 +3,14 @@
 /**
  * binary_tree_depth - Calculate the depth of a node in a binary tree.
  * @tree: A pointer to the node for which you want to calculate the depth.
- * Return: The depth of the node, or -1 if the node is NULL.
+ * Return: The depth of the node, or 0 if the node is NULL.
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
 
 	if (!tree)
-		return (-1);
+		return (0);
 
 	while (tree->parent != NULL)
 	{
