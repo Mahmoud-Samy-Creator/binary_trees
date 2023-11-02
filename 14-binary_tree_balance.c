e4c63961b4825b66241da73058e7694ce253e629
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-11-03 01:27:29 +0200

    fix betty task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,12 +10,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height, right_height;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	return (left_height > right_height ? left_height : right_height) + 1;
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
 /**
@@ -26,7 +26,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return 0;
+		return (0);
 
-	return (int)binary_tree_height(tree->left) - (int)binary_tree_height(tree->right);
+	return ((int)binary_tree_height(tree->left) -
+		(int)binary_tree_height(tree->right));
 }
