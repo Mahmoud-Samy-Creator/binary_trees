942406becc0333cb67ab5821bddf6980e2b0dfbd
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-11-03 01:44:08 +0200

    add task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+
 /**
  * binary_tree_uncle - finds the uncle of a node
  * @node: a pointer to the node to find the uncle
@@ -15,6 +16,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 	if (grandparent->left == node->parent)
 		return (grandparent->right);
+
 
 	return (grandparent->left);
 }
