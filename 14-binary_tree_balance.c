28c9ee473b147350242b60a358016c3225139e1c
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-02 05:45:50 +0000

    14. Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,15 +7,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (tree == NULL)
-        return 0;
+	if (tree == NULL)
+		return 0;
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    return (left_height > right_height ? left_height : right_height) + 1;
+	return (left_height > right_height ? left_height : right_height) + 1;
 }
 
 /**
@@ -25,8 +25,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
+	if (tree == NULL)
+		return 0;
 
-    return (int)binary_tree_height(tree->left) - (int)binary_tree_height(tree->right);
+	return (int)binary_tree_height(tree->left) - (int)binary_tree_height(tree->right);
 }
